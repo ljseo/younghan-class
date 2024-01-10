8b3f2aa5685ab456186236aa7a3fed51a3f35d9d
이준서 <wnstj7866@naver.com>
2024-01-10 22:58:47 +0900

    springdatajpa session2

--- springdatajpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ springdatajpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,22 +1,40 @@
 package study.datajpa.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username","age"})
 public class Member {
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
-    private String name;
+    private String username;
+    private int age;
 
-    protected Member(){} // jpa이기 때문에 필요함
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String name) {
-        this.name = name;
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+
+
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
